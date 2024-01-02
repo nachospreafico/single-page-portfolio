11879f7f4cdaf9d4d2226217d6e4085aa8653d08
Ignacio Spreafico <nachospreafico06@gmail.com>
2024-01-02 12:48:27 +0100

    changed font size for table and desktop on skills component

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,7 +1,7 @@
 const SkillItem = ({ title, exp }) => {
   return (
     <div className="flex flex-col gap-2 items-start">
-      <h2 className="text-[32px]  text-white font-bold md:text-left leading-10 tracking-[1px]">
+      <h2 className="text-[32px] md:text-[48px]  text-white font-bold md:text-left leading-10 md:leading-[56px] tracking-[-1px] md:tracking-[-1.5px]">
         {title}
       </h2>
       <p className="text-[#D9D9D9] font-medium leading-6 ">{exp}</p>
@@ -40,7 +40,7 @@ const Skills = () => {
   return (
     <section
       id="skill"
-      className="flex flex-col items-center justify-center gap-6 skill-section md:py-7 xl:py-10 md:px-[30px] lg:px-[90px] xl:px-[165px]"
+      className="flex flex-col  items-center justify-center gap-6 skill-section md:py-7 xl:py-10 md:px-[30px] lg:px-[90px] xl:px-[165px]"
     >
       {skills.map((elem, index) => {
         return <SkillItem title={elem.title} key={index} exp={elem.exp} />;
